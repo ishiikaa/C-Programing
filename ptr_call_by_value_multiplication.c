2c75303760b4d833942ef87b60a477316cda7c23
ishiikaa <ishiikaa>
2021-10-06 15:37:15 +0530

    first commit

---
+++ ptr_call_by_value_multiplication.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int call(int a);
+
+    int main(){
+     int a = 3;
+    call(a);
+     
+    return 0;
+    }
+int call(int a){
+    printf("The value of %d\n", a * (10));
+   return a;
+   
+    
+   
+}
